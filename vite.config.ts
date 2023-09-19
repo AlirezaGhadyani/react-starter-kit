14cee9cfbcdc1396a5ce050f2fb9cca125b459b0
AlirezaGhadyani <alirezaghadyani16@gmail.com>
2023-09-19 12:33:29 +0430

    init: create base folder structure and config absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      $: "/public",
+      "@": "/src",
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@constants": "/src/constants",
+      "@features": "/src/features",
+      "@hooks": "/src/hooks",
+      "@libs": "/src/libs",
+      "@pages": "/src/pages",
+      "@types": "/src/types",
+      "@utils": "/src/utils",
+    },
+  },
+});
